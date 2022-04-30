511ef76ded6ed66cef23377858e5949538feaf00
BOBOLA <w0772193@myscc.ca>
2022-04-29 21:36:36 -0400

    Generated Header, Title & Footer for Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TitleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
