ff0f3e3c4cf1d094b74aba85362f8e2dc0fd592f
BOBOLA <w0772193@myscc.ca>
2022-04-30 01:23:50 -0400

    Included Tools and WorkCategory Section

---
+++ src/app/work-category/work-category.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WorkCategoryComponent } from './work-category.component';
+
+describe('WorkCategoryComponent', () => {
+  let component: WorkCategoryComponent;
+  let fixture: ComponentFixture<WorkCategoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WorkCategoryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WorkCategoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
