91bcc2878a43c320b5eac2151b37ac41f3b80d84
BOBOLA <w0772193@myscc.ca>
2022-06-02 22:14:30 -0400

    Projects, Footer & About + Added Socialmedia images to footer + Changed about me text + Included Project Images and descriptions including link to projects etc

--- src/app/work-category/work-category.component.ts
+++ src/app/work-category/work-category.component.ts
@@ -11,7 +11,53 @@ export class WorkCategoryComponent implements OnInit {
  project : Projects[];
 
   constructor() {
-    this.project = [];
+    this.project = [
+      //Json type format
+      //Use this in html if content image is not existing <img *ngIf="content?.imgURL" (click)="logId()" [src]="content?.imgURL">
+      {
+        link: "",
+        image: "assets/Projects/enactus-thumb.png",
+        title: "Enactus St Clair",
+        description: "Enactus is an international organization that connects student, academic and business leaders through entrepreneurial-based projects that empower people to transform opportunities into real, sustainable progress for themselves and their communities. .",
+      },
+      {
+        link: "",
+        image: "assets/Projects/coincheck-thumb.png",
+        title: "Coin Check ",
+        description: "CoinCheck is an app that allows users to keep track of their favourite Cryptocurrencies. The app also allows users to make investment calculations and provide estimates on taxes..",
+      },
+      {
+        link: "",
+        image: "https://coincheck-8326f.web.app/assets/ipadCropped.png",
+        title: "Coin Check",
+        description: "CoinCheck is an app that allows users to keep track of their favourite Cryptocurrencies. The app also allows users to make investment calculations and provide estimates on taxes..",
+  },      {
+        link: "",
+        image: "assets/Projects/enactus-thumb.png",
+        title: "Enactus St Clair",
+        description: "Enactus is an international organization that connects student, academic and business leaders through entrepreneurial-based projects that empower people to transform opportunities into real, sustainable progress for themselves and their communities. .",
+      },
+      {
+        link: "",
+        image: "assets/Projects/coincheck-thumb.png",
+        title: "Coin Check ",
+        description: "CoinCheck is an app that allows users to keep track of their favourite Cryptocurrencies. The app also allows users to make investment calculations and provide estimates on taxes..",
+      },
+      {
+        link: "",
+        image: "assets/Projects/enactus-thumb.png",
+        title: "Enactus St Clair",
+        description: "Enactus is an international organization that connects student, academic and business leaders through entrepreneurial-based projects that empower people to transform opportunities into real, sustainable progress for themselves and their communities. .",
+      },
+      {
+        link: "",
+        image: "assets/Projects/coincheck-thumb.png",
+        title: "Coin Check ",
+        description: "CoinCheck is an app that allows users to keep track of their favourite Cryptocurrencies. The app also allows users to make investment calculations and provide estimates on taxes..",
+      },
+
+
+    ];
 
   }
 
