c0651b1f51d9520fe4f278e55224dfc0a44a1eb4
BOBOLA <w0772193@myscc.ca>
2022-09-26 19:53:01 -0400

    Some Aditional changes to design and presentation

--- src/app/work-category/work-category.component.ts
+++ src/app/work-category/work-category.component.ts
@@ -15,21 +15,21 @@ export class WorkCategoryComponent implements OnInit {
       //Json type format
       //Use this in html if content image is not existing <img *ngIf="content?.imgURL" (click)="logId()" [src]="content?.imgURL">
       {
-        link: "",
+        link: "https://enactus-test.netlify.app",
         image: "assets/Projects/enactus-thumb.png",
         title: "Enactus St Clair",
         description: "Enactus is an international organization that connects student, academic and business leaders through entrepreneurial-based projects that empower people to transform opportunities into real, sustainable progress for themselves and their communities. .",
       },
       {
-        link: "",
+        link: "https://coincheck-8326f.web.app",
         image: "assets/Projects/coincheck-thumb.png",
         title: "Coin Check ",
         description: "CoinCheck is an app that allows users to keep track of their favourite Cryptocurrencies. The app also allows users to make investment calculations and provide estimates on taxes..",
       },
       {
-        link: "",
-        image: "https://coincheck-8326f.web.app/assets/ipadCropped.png",
-        title: "Coin Check",
+        link: "https://jerafcard-50782.web.app",
+        image: "assets/Projects/Jeraf.png",
+        title: "Jeraf",
         description: "CoinCheck is an app that allows users to keep track of their favourite Cryptocurrencies. The app also allows users to make investment calculations and provide estimates on taxes..",
   },      {
         link: "",
